7db54dfbabd87e258adbc5965ee11c6bb59e5fc3
jorthi <joris.thiery@smile.fr>
2023-04-27 20:04:46 +0200

    feat: inject var

--- step.sh
+++ step.sh
@@ -9,7 +9,7 @@ sonar_host_url="${sonar_host_url}"
 sonar_login="${sonar_login}"
 
 # launch script
-/usr/bin/python3 -c "$(wget -q -O - https://raw.githubusercontent.com/neopixl/bitrise-step-sonarqube-ios/feature/new-sonnar-scanner/sonnar-ios-step.py)"
+/usr/bin/python3 -c "$(wget -q -O - https://raw.githubusercontent.com/neopixl/bitrise-step-sonarqube-ios/feature/new-sonnar-scanner/sonnar-ios-step.py $xcode_project_path $xcode_workspace_path $app_scheme $sonar_project_key $sonar_host_url $sonar_login)"
 
 
 
