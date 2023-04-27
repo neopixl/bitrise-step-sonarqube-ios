9facd70d6842ab9e3c4821cbdaceb4cd62e64c3b
jorthi <joris.thiery@smile.fr>
2023-04-27 16:37:03 +0200

    feat: add some logs

--- sonnar-ios-step.py
+++ sonnar-ios-step.py
@@ -3,7 +3,7 @@ import os
 print("\n\n\n\n\n* * * * * * * *      STARTING NEOPIXL SONAR SCRIPT       * * * * * * * * * \n\n\n\n\n", flush=True)
 
 # Install all dependencies
-print("\n\n\n\n\n* * * * * * * *      Install all dependencies       * * * * * * * * * \n")
+print("\n\n\n\n\n* * * * * * * *      Install all dependencies       * * * * * * * * * \n", flush=True)
 
 os.system("pip3 install mobsfscan");
 
@@ -19,12 +19,12 @@ scheme = "Visa-env_DEV"
 sonar_host_url = "https://sonar.smile.fr"
 sonar_login = "a94185abb45a1db7dd9f2f14d101a6eaf0b7b9c0"
 
-print("	    sonar project name 		= %s" % sonar_project_name)
-print("	    project_root_path 		= %s" % project_root_path)
-print("	    xcodeproj_path 			= %s" % xcodeproj_path)
-print("	    xcworkspace_path 		= %s" % xcworkspace_path)
-print("	    scheme 					= %s" % scheme)
-print("	    sonar_host_url 			= %s" % sonar_host_url)
+print("	    sonar project name 		= %s" % sonar_project_name, flush=True)
+print("	    project_root_path 		= %s" % project_root_path, flush=True)
+print("	    xcodeproj_path 			= %s" % xcodeproj_path, flush=True)
+print("	    xcworkspace_path 		= %s" % xcworkspace_path, flush=True)
+print("	    scheme 					= %s" % scheme, flush=True)
+print("	    sonar_host_url 			= %s" % sonar_host_url, flush=True)
 
 # Prepare sonar-scanner options
 print("\n\n\n\n\n* * * * * * * *      Prepare sonar-scanner options       * * * * * * * * * \n", flush=True)
@@ -43,7 +43,7 @@ sonar_scanner_cmd += "-Dsonar.exclusions=**/*.xml,Pods/**/*,Reports/**/*,**/Sour
 sonar_scanner_cmd += "-Dsonar.sources='%s' " % project_root_path
 
 # Dependency Check (security hotspot)
-print("\n\n\n\n\n* * * * * * * *      Add Dependency-check to sonar options       * * * * * * * * * \n, flush=True")
+print("\n\n\n\n\n* * * * * * * *      Add Dependency-check to sonar options       * * * * * * * * * \n", flush=True)
 #os.system("xcodebuild -resolvePackageDependencies"); #generate spm package.resolved used for dependency check TODO: need that ?
 dep_check_cmd = "dependency-check --enableExperimental --project %s --format JSON --format HTML --scan %s/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" % (xcodeproj_path, xcodeproj_path)
 os.system(dep_check_cmd);
@@ -53,13 +53,13 @@ sonar_scanner_cmd += "-Dsonar.dependencyCheck.summarize=true "
 sonar_scanner_cmd += "-Dsonar.dependencyCheck.securityHotspot=true "
 
 # Verbose
-print("\n\n\n\n\n* * * * * * * *      Verbose to sonar options       * * * * * * * * * \n, flush=True")
+print("\n\n\n\n\n* * * * * * * *      Verbose to sonar options       * * * * * * * * * \n", flush=True)
 #sonar_scanner_cmd += "-X -Dsonar.verbose=true "
 
-print("\n\n\n\n\n* * * * * * * *      RUN SONAR COMMAND       * * * * * * * * * \n, flush=True")
-print("cmd sonar-scanner === %s" % sonar_scanner_cmd)
+print("\n\n\n\n\n* * * * * * * *      RUN SONAR COMMAND       * * * * * * * * * \n", flush=True)
+print("cmd sonar-scanner === %s" % sonar_scanner_cmd, flush=True)
 os.system(sonar_scanner_cmd);
 
 
-print("\n\n\n\n\n* * * * * * * *      FINISHING NEOPIXL SONAR SCRIPT       * * * * * * * * * \n, flush=True")
+print("\n\n\n\n\n* * * * * * * *      FINISHING NEOPIXL SONAR SCRIPT       * * * * * * * * * \n", flush=True)
 
