0a733a5c8833e27da60e29d5119f51b822a4a723
jorthi <joris.thiery@smile.fr>
2023-04-28 10:06:57 +0200

    feat: launch script in local

--- step.sh
+++ step.sh
@@ -8,14 +8,8 @@ sonar_project_key="${sonar_project_key}"
 sonar_host_url="${sonar_host_url}"
 sonar_login="${sonar_login}"
 
-THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-ls -l $THIS_SCRIPT_DIR
-
-chmod +X sonnar-ios-step.py 
-
-python3 sonnar-ios-step.py
-
 # launch script
-#/usr/bin/python3 -c "$(wget -q -O - https://raw.githubusercontent.com/neopixl/bitrise-step-sonarqube-ios/feature/new-sonnar-scanner/sonnar-ios-step.py $xcode_project_path $xcode_workspace_path $app_scheme $sonar_project_key $sonar_host_url $sonar_login)"
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+python3 $THIS_SCRIPT_DIR/sonnar-ios-step.py
 
+#/usr/bin/python3 -c "$(wget -q -O - https://raw.githubusercontent.com/neopixl/bitrise-step-sonarqube-ios/feature/new-sonnar-scanner/sonnar-ios-step.py $xcode_project_path $xcode_workspace_path $app_scheme $sonar_project_key $sonar_host_url $sonar_login)"
