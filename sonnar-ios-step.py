1e10cb3a41729fe1720bbb295e2eef377e7f6d77
jorthi <joris.thiery@smile.fr>
2023-04-28 12:05:54 +0200

    feat: add some logs + env var

--- sonnar-ios-step.py
+++ sonnar-ios-step.py
@@ -1,25 +1,41 @@
 import os
 
-print("\n\n\n\n\n* * * * * * * *      STARTING NEOPIXL SONAR SCRIPT       * * * * * * * * * \n\n\n\n\n", flush=True)
+print("""\n
+███╗   ██╗███████╗ ██████╗ ██████╗ ██╗██╗  ██╗██╗         ███████╗ ██████╗ ███╗   ██╗ █████╗ ██████╗     ███████╗████████╗███████╗██████╗ 
+████╗  ██║██╔════╝██╔═══██╗██╔══██╗██║╚██╗██╔╝██║         ██╔════╝██╔═══██╗████╗  ██║██╔══██╗██╔══██╗    ██╔════╝╚══██╔══╝██╔════╝██╔══██╗
+██╔██╗ ██║█████╗  ██║   ██║██████╔╝██║ ╚███╔╝ ██║         ███████╗██║   ██║██╔██╗ ██║███████║██████╔╝    ███████╗   ██║   █████╗  ██████╔╝
+██║╚██╗██║██╔══╝  ██║   ██║██╔═══╝ ██║ ██╔██╗ ██║         ╚════██║██║   ██║██║╚██╗██║██╔══██║██╔══██╗    ╚════██║   ██║   ██╔══╝  ██╔═══╝ 
+██║ ╚████║███████╗╚██████╔╝██║     ██║██╔╝ ██╗███████╗    ███████║╚██████╔╝██║ ╚████║██║  ██║██║  ██║    ███████║   ██║   ███████╗██║     
+╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝    ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚══════╝   ╚═╝   ╚══════╝╚═╝                                                                                                                                                                                                                    
+\n\n""", flush=True)
 
 # Install all dependencies
-print("\n\n\n\n\n* * * * * * * *      Install all needed dependencies       * * * * * * * * * \n", flush=True)
+print("""\n\n
+ ___       __ ___                   _   _   __ _ 
+  |  |\ | (_   |  /\  |  |    |\/| / \ |_) (_  |_ 
+ _|_ | \| __)  | /--\ |_ |_   |  | \_/ |_) __) |
+\n""", flush=True)
 
-os.system("pip3 install mobsfscan");
+os.system("pip3 install mobsfscan --quiet");
 
 project_root_path = "/Users/vagrant/git"
 
 # Retrieve all user injected variables
 print("\n\n\n\n\n* * * * * * * *      Retrieve all user injected variables       * * * * * * * * * \n", flush=True)
 
+#project configuration
 xcodeproj_path = "%s/%s" % (project_root_path, os.getenv('xcode_project_path'))
-xcworkspace_path = "%s/%s" % (project_root_path, os.getenv('xcode_workspace_path'))
+xcworkspace_path = "%s/%s" % (project_root_path, os.getenv('xcode_workspace_path')) #todo: use it when needed
 scheme = os.getenv('app_scheme')
+
+#sonar server configuration
 sonar_project_name = os.getenv('sonar_project_key')
 sonar_host_url = os.getenv('sonar_host_url')
 sonar_login = os.getenv('sonar_login')
 
-print("\n\n\n\n\n* * * * * * * *   TEST_TEST_TEST %s %s \n",(xcodeproj_path, scheme))
+#other configuration
+verbose_mode_enabled = os.getenv('verbose_mode_enabled')
+exclusion_file = os.getenv('exclusion_file')
 
 # Prepare sonar-scanner options
 print("\n\n\n\n\n* * * * * * * *      Prepare sonar-scanner options       * * * * * * * * * \n", flush=True)
@@ -34,7 +50,7 @@ sonar_scanner_cmd += "-Dsonar.login=%s " % sonar_login
 print("\n\n\n\n\n* * * * * * * *      Add project config to sonar options       * * * * * * * * * \n", flush=True)
 sonar_scanner_cmd += "-Dsonar.apple.project=%s " % xcodeproj_path
 sonar_scanner_cmd += "-Dsonar.projectKey=%s " % sonar_project_name
-sonar_scanner_cmd += "-Dsonar.exclusions=**/*.xml,Pods/**/*,Reports/**/*,**/SourcePackages/checkouts/**/*,**/*.html "
+sonar_scanner_cmd += "-Dsonar.exclusions=%s " % exclusion_file
 sonar_scanner_cmd += "-Dsonar.sources='%s' " % project_root_path
 
 # Dependency Check (security hotspot)
@@ -48,12 +64,19 @@ sonar_scanner_cmd += "-Dsonar.dependencyCheck.securityHotspot=true "
 
 # Verbose
 print("\n\n\n\n\n* * * * * * * *      Verbose to sonar options       * * * * * * * * * \n", flush=True)
-#sonar_scanner_cmd += "-X -Dsonar.verbose=true "
+if verbose_mode_enabled == 'on':
+	sonar_scanner_cmd += "-X -Dsonar.verbose=true "
 
 print("\n\n\n\n\n* * * * * * * *      RUN SONAR COMMAND       * * * * * * * * * \n", flush=True)
-print("cmd sonar-scanner == %s" % sonar_scanner_cmd, flush=True)
+print("Final cmd sonar-scanner == %s" % sonar_scanner_cmd, flush=True)
 os.system(sonar_scanner_cmd);
 
 
-print("\n\n\n\n\n* * * * * * * *      FINISHING NEOPIXL SONAR SCRIPT       * * * * * * * * * \n", flush=True)
-
+print("""\n\n
+███████╗███╗   ██╗██████╗ 
+██╔════╝████╗  ██║██╔══██╗
+█████╗  ██╔██╗ ██║██║  ██║
+██╔══╝  ██║╚██╗██║██║  ██║
+███████╗██║ ╚████║██████╔╝
+╚══════╝╚═╝  ╚═══╝╚═════╝ 
+\n""", flush=True)
