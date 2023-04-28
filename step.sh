e7cdba19d1cd002d158d1a811822d321ead699b1
jorthi <joris.thiery@smile.fr>
2023-04-28 09:23:09 +0200

    feat: try to launch sonar

--- step.sh
+++ step.sh
@@ -8,8 +8,13 @@ sonar_project_key="${sonar_project_key}"
 sonar_host_url="${sonar_host_url}"
 sonar_login="${sonar_login}"
 
+chmod +X sonnar-ios-step.py 
+
+
+python3 sonnar-ios-step.py
+
 # launch script
-/usr/bin/python3 -c "$(wget -q -O - https://raw.githubusercontent.com/neopixl/bitrise-step-sonarqube-ios/feature/new-sonnar-scanner/sonnar-ios-step.py $xcode_project_path $xcode_workspace_path $app_scheme $sonar_project_key $sonar_host_url $sonar_login)"
+#/usr/bin/python3 -c "$(wget -q -O - https://raw.githubusercontent.com/neopixl/bitrise-step-sonarqube-ios/feature/new-sonnar-scanner/sonnar-ios-step.py $xcode_project_path $xcode_workspace_path $app_scheme $sonar_project_key $sonar_host_url $sonar_login)"
 
 
 
