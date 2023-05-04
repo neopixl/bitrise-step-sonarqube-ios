3d88b798bbd634a8d873633e96cf81db60a964ef
jorthi <joris.thiery@smile.fr>
2023-05-04 10:27:41 +0200

    feat: function for unit test

--- sonnar-ios-step.py
+++ sonnar-ios-step.py
@@ -138,17 +138,18 @@ def runUnitTest(xcodeprojPath, schemeName):
     print("\n-> Run unit test \n", flush=True)
     print("\n-> First, build the project \n", flush=True)
     xcodebuild_cmd = "xcrun xcodebuild "
-	xcodebuild_cmd += "-project %s " % xcodeprojPath
-	xcodebuild_cmd += "-scheme %s " % schemeName
-	xcodebuild_cmd += "-sdk iphonesimulator "
-	xcodebuild_cmd += "-destination 'platform=iOS Simulator,name=iPhone 14 Plus' "
-	#xcodebuild_cmd += "-derivedDataPath './derivedData' "
-	xcodebuild_cmd += "-resultBundlePath 'build/result.xcresult' "
-	xcodebuild_cmd += "-quiet "
-	xcodebuild_cmd += "clean test"
-	print("xcodebuild_cmd === %s" % xcodebuild_cmd)
-	os.system(xcodebuild_cmd);
+    xcodebuild_cmd += "-project %s " % xcodeprojPath
+    xcodebuild_cmd += "-scheme %s " % schemeName
+    xcodebuild_cmd += "-sdk iphonesimulator "
+    xcodebuild_cmd += "-destination 'platform=iOS Simulator,name=iPhone 14 Plus' "
+    #xcodebuild_cmd += "-derivedDataPath './derivedData' "
+    xcodebuild_cmd += "-resultBundlePath 'build/result.xcresult' "
+    xcodebuild_cmd += "-quiet "
+    xcodebuild_cmd += "clean test"
+    print("xcodebuild_cmd === %s" % xcodebuild_cmd)
+    os.system(xcodebuild_cmd);
 
+	
 
 
 
