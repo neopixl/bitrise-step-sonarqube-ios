1ef5634f1069dcba714347b6f084ecc72b13dbf6
jorthi <joris.thiery@smile.fr>
2023-11-02 12:46:13 +0100

    feat: add some options

--- sonnar-ios-step.py
+++ sonnar-ios-step.py
@@ -57,7 +57,11 @@ print("\n-> Add other configuration\n", flush=True)
 verbose_mode_enabled = os.getenv('verbose_mode_enabled')
 exclusion_file = os.getenv('exclusion_file')
 run_unit_test = os.getenv('run_unit_test')
+run_dcheck = os.getenv('run_dcheck')
+run_dtrack = os.getenv('run_dtrack')
+run_periphery = os.getenv('run_periphery')
 target_name = os.getenv('target_name')
+extra_sonar_param = os.getenv('extra_sonar_param')
 
 # Build project
 print("""\n\n
@@ -122,48 +126,53 @@ if run_unit_test == "on":
     #sonar.apple.resultBundlePath=custom/path/to/file.xcresult # Defaults to build/result.xcresult
 
 # Dependency Check (security hotspot)
-print("\n-> Add Dependency-check to sonar options \n", flush=True)
+if run_dcheck == "on":
+    print("\n-> Add Dependency-check to sonar options \n", flush=True)
 
-pod_scan_option = ""
-spm_scan_option = ""
+    pod_scan_option = ""
+    spm_scan_option = ""
 
-is_SPM_Exist = os.path.exists("%s/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" % xcodeproj_path) 
-print("\n-> SPM (Package.resolved) file exist : %s \n" % is_SPM_Exist, flush=True)
+    is_SPM_Exist = os.path.exists("%s/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" % xcodeproj_path) 
+    print("\n-> SPM (Package.resolved) file exist : %s \n" % is_SPM_Exist, flush=True)
 
-if is_SPM_Exist == True:
-    spm_scan_option = "--scan %s/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" % xcodeproj_path
+    if is_SPM_Exist == True:
+        spm_scan_option = "--scan %s/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" % xcodeproj_path
 
-if podfile_path != "":
-    pod_scan_option = "--scan %s" % podfile_path
+    if podfile_path != "":
+        pod_scan_option = "--scan %s" % podfile_path
 
-dep_check_cmd = "dependency-check --enableExperimental --project %s --format JSON --format HTML %s %s" % (xcodeproj_path, spm_scan_option, pod_scan_option)
-print("\n-> Launch Dependency-check (to generate report file) cmd %s\n" % dep_check_cmd, flush=True)
-os.system(dep_check_cmd);
-sonar_scanner_cmd += "-Dsonar.dependencyCheck.jsonReportPath=%s/%s " % (project_root_path, "dependency-check-report.json")
-sonar_scanner_cmd += "-Dsonar.dependencyCheck.htmlReportPath=%s/%s " % (project_root_path, "dependency-check-report.html")
-sonar_scanner_cmd += "-Dsonar.dependencyCheck.summarize=true "
-sonar_scanner_cmd += "-Dsonar.dependencyCheck.securityHotspot=true "
+    dep_check_cmd = "dependency-check --enableExperimental --project %s --format JSON --format HTML %s %s" % (xcodeproj_path, spm_scan_option, pod_scan_option)
+    print("\n-> Launch Dependency-check (to generate report file) cmd %s\n" % dep_check_cmd, flush=True)
+    os.system(dep_check_cmd);
+    sonar_scanner_cmd += "-Dsonar.dependencyCheck.jsonReportPath=%s/%s " % (project_root_path, "dependency-check-report.json")
+    sonar_scanner_cmd += "-Dsonar.dependencyCheck.htmlReportPath=%s/%s " % (project_root_path, "dependency-check-report.html")
+    sonar_scanner_cmd += "-Dsonar.dependencyCheck.summarize=true "
+    sonar_scanner_cmd += "-Dsonar.dependencyCheck.securityHotspot=true "
 
 # Periphery (code duplication & dead code)
-print("\n-> Launch Periphery (code duplication & dead code)\n", flush=True)
+if run_periphery == "on":
+    print("\n-> Launch Periphery (code duplication & dead code)\n", flush=True)
 
-sonar_scanner_cmd += "-Dsonar.apple.periphery.schemes=%s " % scheme
-sonar_scanner_cmd += "-Dsonar.apple.periphery.indexStorePath=%s " % "'../derivedData/Index.noindex/DataStore'"
-sonar_scanner_cmd += "-Dsonar.apple.periphery.targets=%s " % target_name
+    sonar_scanner_cmd += "-Dsonar.apple.periphery.schemes=%s " % scheme
+    sonar_scanner_cmd += "-Dsonar.apple.periphery.indexStorePath=%s " % "'../derivedData/Index.noindex/DataStore'"
+    sonar_scanner_cmd += "-Dsonar.apple.periphery.targets=%s " % target_name
 
 # Verbose
+if verbose_mode_enabled == 'on':
+    sonar_scanner_cmd += "-X -Dsonar.verbose=true "
+
+# Extra sonar scanner parameters
+sonar_scanner_cmd += "%s" % extra_sonar_param
+
 print("""\n\n
   _             __                   _       _  
  |_)     ._    (_   _  ._   _. ._   /  |\/| | \ 
  | \ |_| | |   __) (_) | | (_| |    \_ |  | |_/
  \n""", flush=True)
-if verbose_mode_enabled == 'on':
-	sonar_scanner_cmd += "-X -Dsonar.verbose=true "
 
 print("\n-> Final cmd sonar-scanner == %s\n\n" % sonar_scanner_cmd, flush=True)
 os.system(sonar_scanner_cmd);
 
-"""
 # Dependency Track (third party libraries)
 print("""\n\n
   _____                            _                         _______             _    
@@ -177,71 +186,69 @@ print("""\n\n
 
  \n""", flush=True)
 
-# Retrieve Package.resolved and transform it to bom.json
-package_path = "%s/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" % xcodeproj_path
+if run_dtrack == "on"
+    # Retrieve Package.resolved and transform it to bom.json
+    package_path = "%s/project.xcworkspace/xcshareddata/swiftpm/Package.resolved" % xcodeproj_path
 
-print("read package at path : %s" % package_path, flush=True)
+    print("read package at path : %s" % package_path, flush=True)
 
-components = []
-package_json = open(package_path)
-data = json.load(package_json)
-for i in data['pins']:
+    components = []
+    package_json = open(package_path)
+    data = json.load(package_json)
+    for i in data['pins']:
 
-    #generate purl
-    purl = i['identity']
-    purl += "@"
-    purl += i['state']['version']
+        #generate purl
+        purl = i['identity']
+        purl += "@"
+        purl += i['state']['version']
 
-    #generate CPE (vulnerabilities) thanks to DCheck report..."
-    dcheck_report_json = open("dependency-check-report.json") 
-    dcheck_report_data = json.load(dcheck_report_json)
+        #generate CPE (vulnerabilities) thanks to DCheck report..."
+        dcheck_report_json = open("dependency-check-report.json") 
+        dcheck_report_data = json.load(dcheck_report_json)
 
-    cpe_id = "none"
+        cpe_id = "none"
 
-    for f in dcheck_report_data['dependencies']:
-        filenameArray = f['fileName'].split(":")
-        if filenameArray[0] == i['identity']:
-            if not (f.get('vulnerabilityIds') is None):
-                cpe_id = f['vulnerabilityIds'][0]['id']
-    
-    externalReferences = [{"url": i['location'], "type": "vcs"}]
+        for f in dcheck_report_data['dependencies']:
+            filenameArray = f['fileName'].split(":")
+            if filenameArray[0] == i['identity']:
+                if not (f.get('vulnerabilityIds') is None):
+                    cpe_id = f['vulnerabilityIds'][0]['id']
+        
+        externalReferences = [{"url": i['location'], "type": "vcs"}]
 
-    package_dict = {
-    "type": "library",
-    "name": i['identity'],
-    "version": i['state']['version'],
-    "purl": purl,
-    "cpe": cpe_id,
-    "externalReferences": externalReferences
+        package_dict = {
+        "type": "library",
+        "name": i['identity'],
+        "version": i['state']['version'],
+        "purl": purl,
+        "cpe": cpe_id,
+        "externalReferences": externalReferences
+        }
+
+        components.append(package_dict)
+
+    package_json.close()
+
+    sbom_dict = {
+        "bomFormat": "CycloneDX",
+        "version": 1,
+        "specVersion": "1.5",
+        "components": components
     }
 
-    components.append(package_dict)
+    print("\n-> generated sbom = %s\n" % sbom_dict, flush=True)
 
-package_json.close()
-
-sbom_dict = {
-    "bomFormat": "CycloneDX",
-    "version": 1,
-    "specVersion": "1.5",
-    "components": components
-}
-
-print("\n-> generated sbom = %s\n" % sbom_dict, flush=True)
-
-# send sbom to dtrack
-headers = {
-    'Content-Type': 'multipart/form-data',
-    'X-Api-Key': 'dtrackAPIKey'
-}
-files = {
-    'autoCreate': (None, 'true'),
-    'projectName': (None, os.getenv('project_key', '')),
-    'projectVersion': (None, os.getenv('projet_version', ''))
-}
-response = requests.post('$dtrackBaseUrl/api/v1/bom', json=sbom_dict, headers=headers, files=files)
-"""
-
-print(response)
+    # send sbom to dtrack
+    headers = {
+        'Content-Type': 'multipart/form-data',
+        'X-Api-Key': 'dtrackAPIKey'
+    }
+    files = {
+        'autoCreate': (None, 'true'),
+        'projectName': (None, os.getenv('project_key', '')),
+        'projectVersion': (None, os.getenv('projet_version', ''))
+    }
+    response = requests.post('$dtrackBaseUrl/api/v1/bom', json=sbom_dict, headers=headers, files=files)
 
 print("""\n\n
 ███████╗███╗   ██╗██████╗ 
