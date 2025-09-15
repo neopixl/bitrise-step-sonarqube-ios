054dd6a4822cd95d384b7168baa93ff2bcdf7287
joris-neopixl <57394383+joris-neopixl@users.noreply.github.com>
2025-09-15 17:49:00 +0200

    Update sonnar-ios-step.py

--- sonnar-ios-step.py
+++ sonnar-ios-step.py
@@ -227,6 +227,8 @@ sonar_scanner_cmd += "-Dsonar.projectVersion=%s " % projet_version
 if run_unit_test == "on":
     print("\n-> Setup unit test in sonar cmd \n", flush=True)
     sonar_scanner_cmd += "-Dsonar.apple.resultBundlePath=build/result.xcresult "
+	sonar_scanner_cmd += "-Dsonar.tests=BFxTXTests "
+	sonar_scanner_cmd += "-Dsonar.tests=BFxTXUITests "
 
 # Dependency Check (security hotspot)
 if run_dcheck == "on":
